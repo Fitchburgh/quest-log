6957f17b1294266a46f9ccbfe871997adaf9cf6e
Gabriel Estes <gabriel.estes@gmail.com>
2016-09-29 17:25:20 -0400

    Adding function implemented.

--- quest.js
+++ quest.js
@@ -2,3 +2,65 @@ $( function() {
   $( "#sortable" ).sortable();
   $( "#sortable" ).disableSelection();
 } );
+
+var addButton = $('.add-button')
+var questLog = $('.quest-log')
+var addForm = $('.add-form')
+
+
+
+// var sortable = $('.sortable')
+
+var questList = document.getElementById('sortable');
+
+var embarkQuest = $('#embark-quest')
+
+var messageId = $('#message_id')
+
+embarkQuest.click(function() {
+  var quest = messageId.val();
+
+  $('<li>').text(quest).appendTo(questList)
+
+  messageId.val('')
+
+  questLog.show();
+  addForm.hide();
+
+  return false;
+})
+
+
+$(function() {
+   $("li").draggable();
+
+   $('#remove').droppable({
+       drop: function(event, ui) {
+           ui.draggable.remove();
+       }
+   });
+});
+
+
+
+
+questLog.show();
+addForm.hide();
+
+addButton.click(function() {
+  questLog.hide();
+  addForm.show();
+});
+
+
+
+
+
+
+
+
+
+
+// // #message_id use data here to add to list
+// localstorage.setItem("quest", $('.message_id'))
+// document.getElementById($(".sortable")).innerHTML = localstorage.getItem("quest")
